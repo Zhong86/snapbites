7a0d3fc948b6b45ba26ee7f1f1894f2a9ba6c841
Zhong86 <billy.zhong0725@gmail.com>
2026-07-22 10:03:24 +0700

    init

---
+++ SnapBites/SnapBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapBitesApp.swift
+//  SnapBites
+//
+//  Created by Mac on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SnapBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
