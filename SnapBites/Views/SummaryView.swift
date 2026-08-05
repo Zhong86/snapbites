e8e2cde99f6a7c0420e5f3ee84ba5c4983da1ab9
Ivan <ivantjondro02@gmail.com>
2026-08-05 08:30:26 +0700

    test

--- SnapBites/Views/SummaryView.swift
+++ SnapBites/Views/SummaryView.swift
@@ -3,20 +3,22 @@ import SwiftUI
 struct SummaryView: View {
     var body: some View {
         VStack {
-            HStack {
-                Image(systemName: "globe").padding(12).background(Color.blue)
-                VStack {
-                    HStack {
-                        Text("Title")
-                        Spacer()
-                        Text("2023-12-12")
+            VStack {
+                HStack {
+                    Image(systemName: "globe").padding(12).background(Color.blue)
+                    VStack {
+                        HStack {
+                            Text("Title")
+                            Spacer()
+                            Text("2023-12-12")
+                        }
+                        HStack {
+                            Text("Description")
+                            Spacer()
+                        }
                     }
-                    HStack {
-                        Text("Description")
-                        Spacer()
-                    }
-                }
-            }.padding(12).background(Color.green)
+                }.padding(12).background(Color.green)
+            }
         }
     }
 }
